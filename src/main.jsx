403763734b0819248b05f643870ee60e51e05334
renngegebanget <ezzbro121@gmail.com>
2026-01-23 02:20:47 +0700

    fix eruda console

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const isDebug =
+  import.meta.env.DEV ||
+  new URLSearchParams(window.location.search).get("debug") === "true";
+
+if (isDebug) {
+  const script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/npm/eruda";
+  document.body.appendChild(script);
+  script.onload = () => eruda.init();
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
